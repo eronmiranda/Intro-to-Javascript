496f84beb43768eb3a62aab8aaa4a8f041b21c65
Eronielle Miranda <emiranda4@nait.ca>
2020-01-31 15:42:31 -0700

    3:41PM 31-JAN-2020 || Missing validation for errors and working on Bonus requirement.

--- assign-1-starter/js/main.js
+++ assign-1-starter/js/main.js
@@ -1,15 +1,17 @@
 // Enter your JavaScript for the solution here...
 let memeForm = document.querySelector("form");
+let selectMeme = memeForm.elements.memeImage;
 
 let memeTopText = document.querySelector(".meme-display .top-text");
 let memeBottomText = document.querySelector(".meme-display .bottom-text");
 
 let memeDisplay = document.querySelector(".content .meme-display img");
 let defaultImgSrc = memeDisplay.getAttribute("src");
+let defaultImgAlt = memeDisplay.getAttribute("alt");
 
 function SubmitHandler(event){
     event.preventDefault();
-    let selectMeme = event.target.elements.memeImage;
+    
     let memeChoice = selectMeme.options[selectMeme.selectedIndex].value;
     let inputTopText = event.target.elements.memeTopText.value;
     let inputBottomText = event.target.elements.memeBottomText.value;
@@ -20,25 +22,30 @@ function SubmitHandler(event){
     }
     else
     {
+        let newAltValue = memeChoice.toString().replace(/-/g," ");
+
         memeChoice = `img/${memeChoice}.png`;
+        
         memeDisplay.setAttribute("src",memeChoice)
+        memeDisplay.setAttribute("alt", newAltValue);
+
         memeTopText.innerHTML = inputTopText;
         memeBottomText.innerHTML = inputBottomText;
     }
 }
 
-function ResetHandler(event){
+function ResetHandler(){
     memeTopText.innerHTML = "";
     memeBottomText.innerHTML = "";
     memeDisplay.setAttribute("src", defaultImgSrc);
+    memeDisplay.setAttribute("alt", defaultImgAlt);
+}
+
+function SelectHandler(event){
+    
 }
 
 
 memeForm.addEventListener("submit", SubmitHandler);
 memeForm.addEventListener("reset", ResetHandler);
-
-
-
-
-//for reset
-//variable.value = '';
+selectMeme.addEventListener("change", SelectHandler);
