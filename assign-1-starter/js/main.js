75d8757a43d8db9528534f87e4363ef238215c92
Eronielle Miranda <emiranda4@nait.ca>
2020-01-31 15:28:06 -0700

    31-JAN-2020 still working on changing alt attribute: removing dashes from a string. Haven't started on bonus

--- assign-1-starter/js/main.js
+++ assign-1-starter/js/main.js
@@ -1,1 +1,44 @@
 // Enter your JavaScript for the solution here...
+let memeForm = document.querySelector("form");
+
+let memeTopText = document.querySelector(".meme-display .top-text");
+let memeBottomText = document.querySelector(".meme-display .bottom-text");
+
+let memeDisplay = document.querySelector(".content .meme-display img");
+let defaultImgSrc = memeDisplay.getAttribute("src");
+
+function SubmitHandler(event){
+    event.preventDefault();
+    let selectMeme = event.target.elements.memeImage;
+    let memeChoice = selectMeme.options[selectMeme.selectedIndex].value;
+    let inputTopText = event.target.elements.memeTopText.value;
+    let inputBottomText = event.target.elements.memeBottomText.value;
+
+    if(memeChoice === "")
+    {
+        prompt("error");
+    }
+    else
+    {
+        memeChoice = `img/${memeChoice}.png`;
+        memeDisplay.setAttribute("src",memeChoice)
+        memeTopText.innerHTML = inputTopText;
+        memeBottomText.innerHTML = inputBottomText;
+    }
+}
+
+function ResetHandler(event){
+    memeTopText.innerHTML = "";
+    memeBottomText.innerHTML = "";
+    memeDisplay.setAttribute("src", defaultImgSrc);
+}
+
+
+memeForm.addEventListener("submit", SubmitHandler);
+memeForm.addEventListener("reset", ResetHandler);
+
+
+
+
+//for reset
+//variable.value = '';
