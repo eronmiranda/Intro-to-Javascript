4f49fe85674976944de7ea87a3585aa490b8f479
Eronielle Miranda <emiranda4@nait.ca>
2020-02-26 12:04:15 -0700

    Finished requirements on Assigment 2

--- assignment2-photo-filter/js/main.js
+++ assignment2-photo-filter/js/main.js
@@ -1,1 +1,32 @@
 // Enter your JavaScript for the solution here...
+let filterBar = document.querySelector(".frm-control");
+let resetBtn = document.querySelector(".reset");
+let thumbDisplay = document.querySelectorAll(".gallery .thumb-display");
+
+filterBar.addEventListener("input", filterHandler);
+
+function filterHandler(evt) {
+  let tagsList = document.querySelectorAll(".thumb-display p.tags");
+  let input = evt.target.value.toLowerCase();
+
+  resetBtn.classList.remove("hidden");
+  for (let index = 0; index < tagsList.length; index++) {
+    if (tagsList[index].innerHTML.match(input)) {
+      thumbDisplay[index].classList.remove("hidden");
+    } else {
+      thumbDisplay[index].classList.add("hidden");
+    }
+  }
+}
+
+resetBtn.addEventListener("click", function(evt) {
+  evt.target.classList.add("hidden");
+  filterBar.value = "";
+  thumbDisplay.forEach(element => element.classList.remove("hidden"));
+});
+
+filterBar.addEventListener("keypress", function(evt) {
+  if (evt.keyCode === 13) {
+    evt.preventDefault();
+  }
+});
